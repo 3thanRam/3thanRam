65bc704e11868a56c2fa7084f9117c5d8f5ceb98
3thanRam <ramsey.e.ethan@gmail.com>
2024-01-27 02:52:56 +0100

    use of list

--- update_readme.py
+++ update_readme.py
@@ -7,7 +7,7 @@ def main():
 
     # Articles data
     list_data = [
-        {"title": "0", "elements": "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"},
+        {"title": "0", "elements": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]},
     ]
 
 
