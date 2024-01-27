a0b47b80217daad21c923ea66ce1b41b11c9727e
3thanRam <ramsey.e.ethan@gmail.com>
2024-01-27 02:55:52 +0100

    bigger list

--- update_readme.py
+++ update_readme.py
@@ -4,11 +4,9 @@ import datetime
 def main():
     # Get the current timestamp in UTC and format it
     #updated_at = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
-
+    N=10
     # Articles data
-    list_data = [
-        {"title": "0", "elements": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]},
-    ]
+    list_data = [{"content": [i for i in range(N)]} for j in range(N)]
 
 
     # Set up Jinja2 environment
