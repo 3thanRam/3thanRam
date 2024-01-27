186e17ce168c1d5b3fe5c565cf9daf29d00ac57f
3thanRam <ramsey.e.ethan@gmail.com>
2024-01-27 02:47:43 +0100

    list template

--- update_readme.py
+++ update_readme.py
@@ -6,22 +6,17 @@ def main():
     #updated_at = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
 
     # Articles data
-    articles_data = [
-        {"title": "My First Blog Post", "url": "https://myblog.com/first-post"},
-        {"title": "Understanding Jinja2", "url": "https://myblog.com/jinja2"}
+    list_data = [
+        {"title": "0", "elements": "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"},
     ]
 
-    weather_data = [
-        {"date": "2023-09-15", "forecast": "Sunny"},
-        {"date": "2023-09-16", "forecast": "Cloudy"}
-    ]
 
     # Set up Jinja2 environment
     env = Environment(loader=FileSystemLoader('templates'))
     template = env.get_template('README.md.j2')
 
     # Render and merge templates
-    rendered_readme = template.render(articles=articles_data, weathers=weather_data)
+    rendered_readme = template.render(list=list_data)
 
     # Save to README.md
     with open("README.md", "w") as f:
